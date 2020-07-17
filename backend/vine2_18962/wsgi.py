2511f8c604b8941beb218c7046311360ba6c961b
Crowdbotics <team@crowdbotics.com>
2020-07-17 05:29:11 +0000

    Update models

--- backend/vine2_18962/wsgi.py
+++ backend/vine2_18962/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vine2_18962.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vine2_18962.settings")
 
 application = get_wsgi_application()
